8afed48288ac4f9dfee81a411a99b8b3eac3af65
Ramesh Singh <Ramesh.Singh@tmotions.com>
2026-08-13 11:43:51 +0530

    solve env issue

--- src/Components/GimiChat/index.js
+++ src/Components/GimiChat/index.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { useState } from "react";
  const keys = process.env.REACT_APP_GEMINI_API_KEY
-console.log("keys>>",keys);
+
 
 const ai = new GoogleGenAI({
   apiKey: keys
